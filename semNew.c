f53637e3bf19d8adf3fe425415ecaac6b1227294
Kyle <latinok@my.erau.edu>
2016-03-20 17:30:56 -0400

    fix build issues

--- semNew.c
+++ semNew.c
@@ -13,11 +13,11 @@
 #include <logLib.h> /* we use logMsg rather than printf */
 
 /* define useful constants for timing */
-#define NANOS_IN_SEC 1000000000
+
+/*#define NANOS_IN_SEC 1000000000
 #define NANOS_PER_MILLI 1000000
 #define TICK sysClkRateGet()/60
-
-/*  */
+*/
 
 /* global variables */
 /* "shared memory": three data to be kept coherent showing the same values when printing */
@@ -27,12 +27,11 @@ int x; int y;int z;
 
 int taskSensor;
 int taskDisplay;
+int ticks=0;
 
 /* semaphores */
-/*
 SEM_Id semBin1;
 SEM_Id semBin2;
-*/
 
 /* declare functions */
 void init(void);
@@ -44,8 +43,8 @@ void init(void){
   semBin2 = semBCreate(SEM_Q_PRIORITY, SEM_EMPTY); /* create binary semaphore requiring display task to wait */
 
   /* spawn tasks */
-  taskSensor = taskSpawn("sens", 95, 0x100, 2000, (FUNCPTR)Sensor, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-  taskDisplay = taskSpawn("disp", 95, 0x100, 2000, (FUNCPTR)Display, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+  taskSensor = taskSpawn("sens", 95, 0x100, 2000, (FUNCPTR)Sensor, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+  taskDisplay = taskSpawn("disp", 95, 0x100, 2000, (FUNCPTR)Display, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 }
 
 /* sensor function atomically increments the values of data */
@@ -66,7 +65,7 @@ void Display(void){
   while(1){
     /* begin critical section */
     semTake(semBin2, WAIT_FOREVER);
-    logMsg("Display #%d=> %d %d %d at %d sec and %d milli_sec\n", count++, data.x ,data.y ,data.z, isec,  milli_sec);
+    logMsg("Display #%d=> %d %d %d at %d ticks\n", count++, data.x ,data.y ,data.z, ticks);
     data.x=0;
     data.y=0;
     data.z=0;
