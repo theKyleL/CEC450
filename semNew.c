ec554abd6d24dd6cfc30ad607af38e30608b8724
Kyle <latinok@my.erau.edu>
2016-03-21 10:00:37 -0400

    finalize
    Removed excess code. Added comments and responses to the end.

--- semNew.c
+++ semNew.c
@@ -1,24 +1,18 @@
-/* Lab 4 part 2
+/*Alejandro Baza & Kyle Latino
+* CEC 450
+* Dr. Kornecki
+* Lab 4 part 2
 *
 * This program is intended to be a practice problem demonstrating the use of semaphores to
 * provide synchronization between tasks.
-*
 */
 
 #include <vxWorks.h> /* Always include this as the first thing in every program */
-/*#include <time.h> /* we use clock_gettime */
 #include <tickLib.h> /* using tickGet() */
 #include <taskLib.h> /* we use tasks */
 #include <sysLib.h> /* we use sysClk... */
 #include <semLib.h> /* we use semaphores */
 #include <logLib.h> /* we use logMsg rather than printf */
-
-/* define useful constants for timing */
-
-/*#define NANOS_IN_SEC 1000000000
-#define NANOS_PER_MILLI 1000000
-#define TICK sysClkRateGet()/60
-*/
 
 /* global variables */
 /* "shared memory": three data to be kept coherent showing the same values when printing */
@@ -59,6 +53,7 @@ void Sensor(void){
     data.y++;
     data.z++;
     semGive(semBin2); /* allow display to run */
+    taskDelay(5);
   }
 }
 
@@ -69,13 +64,19 @@ void Display(void){
     semTake(semBin2, WAIT_FOREVER); /* begin critical section */
     tFin = tickGet();
     ticks = tFin - tIn;
-    logMsg("Display #%d=> %d %d %d at %d ticks\n", count++, data.x ,data.y ,data.z, ticks, ticks /* not sure why */);
+    logMsg("Display #%d=> %d %d %d at %d ticks\n", count++, data.x ,data.y ,data.z, ticks, ticks /* not sure why an additional argument is needed... */);
     data.x=0;
     data.y=0;
     data.z=0;
     semGive(semBin1); /* allow sensor to run */
+    taskDelay(5);
   }
 }
 
 
-/* what number comes next in the sequence? 4, 3, 3, 5, 4, 4, 3, 5, 5, 4, _ */
+/*
+Running this program produces an output of "Display #'count'=> 1 1 1 at 'some number ticks'" to print on the arcom monitor. The program works because we use two binary semaphores, 'semBin1' and 'semBin2', to force the critical sections of the code to alternate. At the end of each critical section the task returns the semaphore for the next task to be allowed to run. This can be acheived without the use of mutex semaphores due to the binary semaphores naturally being made mutually exclusive. The use of the taskDelay function is simply to make the output more readable. Removing the delays will allow the tasks to alternate at their maximum posisble speed, however the output values of x, y, and z will remain 1. VxWorks applies a method of priority inversion when a high priority task is requesting a semaphore currently held by a lower priority task. This is done by temporarily changing the priority of the task in control of the semaphore to match the highest priority task currently waiting for that semaphore. Thus, changing the priority of the tasks will not effect the output.
+
+Comments:
+Looking back it is hard to see why this lab question caused so much trouble. The most difficult part could only be the lack of clarity. This is understandable due to fact that substantial clarification would almost be writing the code for us. As usual, once the algorithm is developed and the code is written it is difficult to imagine writing the code any other way. The solution is simple and makes perfect sense.
+*/
